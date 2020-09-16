6217f3af0ba26a5a25c3b3815e3c3574a1caa764
Habib Shakibanejad <hs_m19@hotmail.com>
2020-09-16 18:59:24 -0400

    Added Sales, message contract, configured nservicebus

---
+++ Messages/MakePayment.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messages
+{
+    public class MakePayment : ICommand
+    {
+        public int Id { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public decimal AmountPaid { get; set; }
+
+    }
+
+    public enum PaymentType
+    {
+        Cash,
+        Credit,
+        Debit
+    }
+}
---
+++ Sales/Program.cs
@@ -0,0 +1,34 @@
+﻿using Messages;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Sales
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "Sales";
+
+            var endpointConfiguration = new EndpointConfiguration("Sales");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+           
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
